b43b0105a8353de03a538afa2fb5abac97f9a877
Funmer <99025570+funmmer@users.noreply.github.com>
2022-10-19 23:37:02 +0300

    Create restream.cc

---
+++ restream.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+namespace sys{
+    void prints(string print){
+     cout << print;
+    }
+    void println(string print){
+     cout << print << endl;
+    }
+    void input(string input){
+     cin >> input;
+    }
+}
